7743179cd738ae66eb92db1ef962a1237f6ce23c
finchhg <finchhg@rose-hulman.edu>
2019-03-25 01:42:21 -0400

    ses14

--- src/m2_console_input.py
+++ src/m2_console_input.py
@@ -3,8 +3,8 @@ This module demonstrates lets you practice INPUT from the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Harrison Finch.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -23,7 +23,6 @@ def double_a_float():
        a. Prompts the user for and inputs a floating point number.
        b. Prints the input number, doubled (i.e., multiplied by 2).
     No input validation is required.  Nothing else should be printed.
-
     Example:
     Here is a sample run, where the user input is to the right
     of the colon:
@@ -31,10 +30,11 @@ def double_a_float():
          -6.28
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    x = float(input('Please enter a float'))
+    print(x * 2)
 
 def print_an_integer_many_times():
     """
@@ -45,7 +45,6 @@ def print_an_integer_many_times():
        b. Prints the input integer, doubled (i.e., multiplied by 2),
           the input number of times.  (See the example.)
     No input validation is required.  Nothing else should be printed.
-
     Example:
     Here are two sample runs, where the user input is to the right
     of the colon:
@@ -53,7 +52,6 @@ def print_an_integer_many_times():
          6
          6
          6
-
          Enter an integer: 5
          10
          10
@@ -62,32 +60,39 @@ def print_an_integer_many_times():
          10
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    x = int(input('Please enter an integer'))
+    a = x * 2
+    for k in range(x):
+        print(a)
 
 def print_an_integer_many_times_on_one_row():
     """
     Same as the previous problem, but print the numbers
     on a single line with no spaces in between them.
-
     Here are two sample runs, where the user input is to the right
     of the colon:
          Enter an integer: 3
          666
-
          Enter an integer: 5
          1010101010
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   The testing code is already written for you (above).
     #
     # HINT: One way to print on a SINGLE line is to build up a string
     #       and then print that (single) string.
     # -------------------------------------------------------------------------
-
+    x = int(input('Please input an integer'))
+    a = 2 * x
+    a = str(a)
+    s = ''
+    for k in range(x):
+        s = s + a
+    print(s)
 
 def input_it_all():
     """
@@ -104,7 +109,6 @@ def input_it_all():
            repeated the input integer number of times.
         b. The string, repeated the input integer number of times.
       No input validation is required.  Nothing else should be printed.
-
     Example:
     Here is a sample run, where the user input is to the right
     of the colons:
@@ -121,10 +125,19 @@ def input_it_all():
          Peace & Love.
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    x = float(input('Enter a positive number: '))
+    y = int(input('Enter a positive integer: '))
+    z = input('Enter a string: ')
 
+    a = x ** 0.5
+
+    for k in range(y):
+        print(a)
+    for k in range(y):
+        print(z)
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
--- src/m4_wait_for_events.py
+++ src/m4_wait_for_events.py
@@ -30,8 +30,8 @@ Ultimately you should be comfortable with both approaches.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Harrison Finch.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
 
@@ -99,15 +99,23 @@ def sum_until_prime_input():
          The sum of the input integers is: 167
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    total = 0
+    number = int(input('Enter a positive integer greater than 1:'))
+    while is_prime(number) is False:
+        total = total + number
+        print('total is:', total)
+        number = int(input('Enter a positive integer greater than 1:'))
+    total = total + number
+    print('total is:', total)
 
 
 def run_test_next_prime():
     """ Tests the   next_prime    function. """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this TEST function.
+    # DONE: 3. Implement this TEST function.
     #   It TESTS the  wait_for_prime  function defined below.
     #   Include at least  ** 6 **  tests. (We supplied 5 tests for you.)
     #
@@ -122,7 +130,7 @@ def run_test_next_prime():
     # Test 1:
     print()
     print('TEST STARTED!  Has it ended?')
-    expected = 7
+    expected = 11
     actual = next_prime(7)
     print('Expected:', expected)
     print('Actual:  ', actual)
@@ -158,20 +166,24 @@ def run_test_next_prime():
     # Test 5:
     print()
     print('TEST STARTED!  Has it ended?')
-    expected = 2
+    expected = 3
     actual = next_prime(2)
     print('Expected:', expected)
     print('Actual:  ', actual)
     print('TEST ENDED!')
 
-    # TODO 3 (continued):
+    # DONE 3 (continued):
     #   PUT YOUR TEST   ** IN THE SPACE BETWEEN **   the
     #   print('TEST STARTED!' ...) and print('TEST ENDED') lines below.
 
     # Test 6:
     print()
     print('TEST STARTED!  Has it ended?')
-
+    expected = 5
+    actual = next_prime(3)
+    print('TEST ENDED!')
+    print('Expected:', expected)
+    print('Actual:  ', actual)
     print('TEST ENDED!')
 
 
@@ -190,13 +202,16 @@ def next_prime(m):
       :type m: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # IMPLEMENTATION REQUIREMENT:
     #    -- Use (call) the   is_prime   function above appropriately.
     # -------------------------------------------------------------------------
-
+    while True:
+        m = m + 1
+        while is_prime(m) is True:
+            return m
 
 def run_test_prime_gap():
     """ Tests the   prime_gap    function. """
@@ -327,19 +342,25 @@ def prime_gap(m):
       :type m: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   The testing code is already written for you (above).
     #
     # IMPLEMENTATION REQUIREMENT:
     #    -- Use (call) the   *** next_prime ***   function
     #       (that you implemented) appropriately.
     # -------------------------------------------------------------------------
+    num1 = 2
+    num2 = 3
+    while num2 - num1 < m:
+        num1 = next_prime(num1)
+        num2 = next_prime(num2)
+    return num1
 
 
 def run_test_wait_for_sum_of_cubes():
     """ Tests the   wait_for_sum_of_cubes    function. """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  wait_for_sum_of_cubes  function defined below.
     #   Include at least  ** 8 **  tests. (We supplied 6 tests for you.)
     #
@@ -405,7 +426,7 @@ def run_test_wait_for_sum_of_cubes():
     print('Actual:  ', actual)
     print('TEST ENDED!')
 
-    # TODO 6 (continued):
+    # DONE 6 (continued):
     #   PUT YOUR TEST   ** IN THE SPACE BETWEEN **   the
     #   print('TEST STARTED!' ...) and print('TEST ENDED') lines below.
     #
@@ -418,16 +439,27 @@ def run_test_wait_for_sum_of_cubes():
 
     print('TEST ENDED!')
 
-    # TODO 6 (continued):
+    # DONE 6 (continued):
     #   PUT YOUR TEST   ** IN THE SPACE BETWEEN **  the
     #   print('TEST STARTED!' ...) and print('TEST ENDED') lines below.
 
     # Test 8:
     print()
     print('TEST STARTED!  Has it ended?')
-
+    expected = 3
+    actual = wait_for_sum_of_cubes(14)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
     print('TEST ENDED!')
 
+    # Test 9:
+    print()
+    print('TEST STARTED!  Has it ended?')
+    expected = 4
+    actual = wait_for_sum_of_cubes(40)
+    print('Expected:', expected)
+    print('Actual:  ', actual)
+    print('TEST ENDED!')
 
 def wait_for_sum_of_cubes(x):
     """
@@ -463,7 +495,7 @@ def wait_for_sum_of_cubes(x):
       :type x: float  [or an int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # IMPLEMENTATION REQUIREMENT:
@@ -473,7 +505,12 @@ def wait_for_sum_of_cubes(x):
     # (or look up) a formula that would allow a faster computation.
     # But no fair using any such approach in this implementation.
     # -------------------------------------------------------------------------
-
+    number = 1
+    cube = number ** 3
+    while cube < x:
+        number = number + 1
+        cube = cube + number ** 3
+    return number
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
